ba045cefc2e9b5f70e51a7453a7ad7cfe5ad2e34
andlo <andlo@outlook.dk>
2018-12-09 15:35:41 +0000

    initial

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import freesound
 
 class SoundLike(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+
+    def Initialize():
+        self.freesound = freesound.FreesoundClient()
+        self.freesound.set_token("<your_api_key>","token")
+
 
     @intent_file_handler('like.sound.intent')
     def handle_like_sound(self, message):
